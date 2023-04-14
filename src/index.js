2f1e1f6cc12cfa7aee76ce803842f0ae43c0dd5b
MacBook Classic <you@example.com>
2023-04-13 20:33:56 -0400

    structured components in separate files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-
-import Counter from "./components/counter";
-
-const App = () => {
-    return <Counter />;
-};
+import App from "./App";
 
 reactDom.render(<App />, document.getElementById("root"));
