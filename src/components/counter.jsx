4f87fa7ef833c294a28f4bfefbfd4126ffab7814
MacBook Classic <you@example.com>
2023-04-15 12:25:34 -0400

    added bin item names

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -20,6 +20,7 @@ const Counter = (props) => {
 
   return (
     <div>
+      <span>{props.name}</span>
       <span className={getBageClasses()}>{formatValue()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
--- src/components/counterList.jsx
+++ src/components/counterList.jsx
@@ -3,14 +3,16 @@ import Counter from "./counter";
 
 const CounterList = () => {
   const counters = [
-    { id: 0, value: 0 },
-    { id: 1, value: 4 },
-    { id: 2, value: 0 },
+    { id: 0, value: 0, name: "Useless item" },
+    { id: 1, value: 4, name: "Spoon" },
+    { id: 2, value: 0, name: "Fork" },
+    { id: 3, value: 0, name: "Plate" },
+    { id: 4, value: 0, name: "Minimalist's kit" },
   ];
   return (
     <>
       {counters.map((count) => (
-        <Counter key={count.id} value={count.value} />
+        <Counter key={count.id} value={count.value} name={count.name} />
       ))}
     </>
   );
